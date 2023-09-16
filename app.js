4c48bf23ad7c148f7aa3d0cc6a7a92989694d9ae
Jack Turner <95172913+turnernator1@users.noreply.github.com>
2023-09-16 10:59:39 +0930

    Initial Commit

---
+++ app.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Flinders CySoc CTF Platform</h1>
+        <p>Welcome to the Capture The Flag challenge!</p>
+      </header>
+      <main>
+        <section>
+          <h2>About Us</h2>
+          <p>CySoc CTF Platform is your gateway to exciting cybersecurity challenges. Join us on a journey of discovery and skill-building in the world of cybersecurity.</p>
+        </section>
+        <section>
+          <h2>Upcoming Challenges</h2>
+          <ul>
+            <li>Web Exploitation</li>
+            <li>Binary Analysis</li>
+            <li>Crypto Challenges</li>
+            <li>Reverse Engineering</li>
+          </ul>
+        </section>
+        <section>
+          <h2>Join the Challenge</h2>
+          <p>Are you ready to test your cybersecurity skills? Join our upcoming challenges and unlock the hidden secrets of the digital world.</p>
+        </section>
+      </main>
+      {/* Hidden flag in the source code */}
+      <footer>
+        <p>Can you uncover hidden secrets? CYSOC{4G3NT_1S_3XP0S3D!}</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
